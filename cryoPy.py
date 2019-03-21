1e3d393a8a0e2eaff475bcf124bd994b6fee5d1a
Eric Keenan <eric.keenan@colorado.edu>
2019-03-20 21:34:23 -0600

    resolve Make cryoPy.py Geo.coords_trans accept 2-D arrays, instead of just 1-D vectors issue

--- cryoPy.py
+++ cryoPy.py
@@ -6,7 +6,7 @@ import numpy as np
 # Class which contains geographic manipulation functions. 
 class Geo:
 
-	# Coordinate system transformation function. 
+	# Coordinate system transformation function.
 	# src_EPSG: Source EPSG coordinate system (ex: MERRA-2 = 4326). 
 	# tgt_EPSG: Target EPSG coordinate system (ex: South polar stereo = 3031).
 	# src_Lat: Numpy 1-D vector array of latitude in src_EPSG to be converted to tgt_EPSG.
@@ -26,11 +26,31 @@ class Geo:
 		tgt_Lat = np.zeros(len(src_Lat))
 		tgt_Lon = np.zeros(len(src_Lon))
 
+		# For transformation, convert 2-D source lat/lon arrays to 1-D vector arrays
+		is_2D = False # Flase if the array is 1-D
+		if type(src_Lat) == list:
+			is_List = True
+
+		elif src_Lat.ndim == 2:
+			is_2D = True
+			shape_2D = src_Lat.shape
+			src_Lat = src_Lat.flatten()
+			src_Lon = src_Lon.flatten()
+			tgt_Lat = np.zeros(src_Lat.shape)
+			tgt_Lon = np.zeros(src_Lon.shape)
+
 		# Perform coordinate transformation.
 		for j in range(0, len(src_Lat)):
-			tgt_coords = transform.TransformPoint(src_Lon[j], src_Lat[j])
+			tgt_coords = transform.TransformPoint(float(src_Lon[j]), float(src_Lat[j]))	
 			tgt_Lon[j] = tgt_coords[0]
 			tgt_Lat[j] = tgt_coords[1]
 
+		# If source Lat/Lon were originally 2D, convert them back to 2D
+		if is_2D:
+			tgt_Lon = tgt_Lon.reshape(shape_2D)
+			tgt_Lat = tgt_Lat.reshape(shape_2D)
+
 		return tgt_Lat, tgt_Lon
+
+
 
